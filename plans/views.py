49997c2ae51746cb52b856e99d6026cdec078502
abrar zahin <abrarzahin@yahoo.com>
2019-07-19 18:07:18 -0400

    14 settings

--- plans/views.py
+++ plans/views.py
@@ -22,16 +22,15 @@ def join(request):
 def checkout(request):
     return render(request, 'plans/checkout.html')
 
-
-
-
+def settings(request):
+    return render(request, 'registration/settings.html')
+def settings(request):
+    return render(request, 'registration/settings.html')
 
     class SignUp(generic.CreateView):
     form_class = CustomSignupForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
-
-    
 
     def form_valid(self, form):
         valid = super(SignUp, self).form_valid(form)
